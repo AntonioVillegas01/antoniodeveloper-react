ba0598e45ffbe16a59ce53f9e1022424a29c31e2
Marco Garcia <macbookpro@MacBook-Pro-de-Macbook.local>
2023-07-26 00:25:04 -0600

    Initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,77 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import bankcoin from "./company/bankcoin.png";
+import yalo from "./company/yalo.png";
+import ozon from "./company/ozon.png";
+import filup from "./company/filup.ico";
+import dotRow from "./company/dotRow.png";
+import ipn from "./company/ipn.png";
+import mgeek from "./company/mgeek.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  mgeek,
+  bankcoin,
+    yalo,
+    ozon,
+    filup,
+    dotRow,
+    ipn,
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,322 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+    bankcoin,
+    dotRow,
+    filup,
+    ipn,
+    ozon,
+    yalo,
+    mgeek
+} from "../assets";
+
+export const navLinks = [
+    {
+        id: "about",
+        title: "About",
+    },
+    {
+        id: "work",
+        title: "Work",
+    },
+    {
+        id: "contact",
+        title: "Contact",
+    },
+];
+
+const services = [
+    {
+        title: "Front-End Developer",
+        icon: web,
+    },
+    {
+        title: "Mobile Developer",
+        icon: mobile,
+    },
+    {
+        title: "Backend Developer",
+        icon: backend,
+    },
+    {
+        title: "Cloud ",
+        icon: creator,
+    },
+];
+
+const technologies = [
+    {
+        name: "HTML 5",
+        icon: html,
+    },
+    {
+        name: "CSS 3",
+        icon: css,
+    },
+    {
+        name: "JavaScript",
+        icon: javascript,
+    },
+    {
+        name: "TypeScript",
+        icon: typescript,
+    },
+    {
+        name: "React JS",
+        icon: reactjs,
+    },
+    {
+        name: "Redux Toolkit",
+        icon: redux,
+    },
+    {
+        name: "Tailwind CSS",
+        icon: tailwind,
+    },
+    {
+        name: "Node JS",
+        icon: nodejs,
+    },
+    {
+        name: "MongoDB",
+        icon: mongodb,
+    },
+    {
+        name: "Three JS",
+        icon: threejs,
+    },
+    {
+        name: "git",
+        icon: git,
+    },
+    {
+        name: "figma",
+        icon: figma,
+    },
+    {
+        name: "docker",
+        icon: docker,
+    },
+];
+
+const experiences = [
+    {
+        title: "Full-Stack Developer",
+        company_name: "OZON",
+        icon: ozon,
+        iconBg: "#383E56",
+        date: "Feb 2022 - Current",
+        points: [
+            "Enhancing User Experience with New Technologies:\n" +
+            "I integrate innovative technologies into web applications, leveraging React.js to improve user experience and satisfaction at Ozon.",
+            "I optimize code, use advanced techniques, and collaborate with the back-end team to ensure lightning-fast load times for web applications at Ozon.",
+            "At Ozon, I lead workshops, provide feedback, and promote best coding practices, ensuring high-quality and maintainable code within the development team.",
+
+        ],
+    },
+    {
+        title: "Full-Stack Developer",
+        company_name: "MoneyGeek",
+        icon: mgeek,
+        iconBg: "#E6DEDD",
+        date: "Sep 2021 - Feb 2022",
+        points: [
+            "Building Scalable JAMstack Applications:\n" +
+            "As a full-stack developer at MoneyGeek, I architect and develop scalable JAMstack applications, utilizing modern JavaScript frameworks to create high-performing web experiences.",
+            "Collaborating with Designers and DevOps:\n" +
+            "I collaborate with designers to bring seamless user interfaces to life and work closely with DevOps teams to ensure efficient deployment and continuous integration for JAMstack projects at MoneyGeek.",
+            "Implementing Serverless Functions for Dynamic Functionality:\n" +
+            "At MoneyGeek, I leverage serverless functions to introduce dynamic functionalities within our JAMstack applications, delivering personalized user experiences while maintaining the benefits of static site generation.",
+        ],
+    },
+    {
+        title: "Back-End Developer at YALO",
+        company_name: "YALO",
+        icon: yalo,
+        iconBg: "#383E56",
+        date: "Jan 2020 - Jan 2021",
+        points: [
+            "Developing Scalable Serverless Backend for Chatbots:\n" +
+            "As a JavaScript back-end developer at Yalo, I design and implement serverless architectures that power chatbots, ensuring scalability and reliability for seamless conversational commerce experiences.",
+            "Leveraging Elasticsearch for Advanced Search and Analytics:\n" +
+            "At Yalo, I harness the power of Elasticsearch to implement robust search functionalities and gather valuable insights from chatbot interactions, enabling data-driven decision-making for optimizing conversational commerce.",
+            "Integrating Third-Party APIs for Enhanced Chatbot Capabilities:\n" +
+            "As part of the Yalo team, I integrate third-party APIs to enrich chatbot capabilities, enabling personalized interactions, natural language processing, and seamless integration with e-commerce platforms, contributing to a more efficient conversational commerce ecosystem.",
+        ],
+    },
+    {
+        title: "Mobile & Full Stack Developer",
+        company_name: "FILUP MX",
+        icon: filup,
+        iconBg: "#E6DEDD",
+        date: "Jan 2019 - Sep 2020",
+        points: [
+            "Creating Feature-Rich MERN Web Application:\n" +
+            "As a developer at FILUP, I played a key role in building a feature-rich human resources web application using the MERN (MongoDB, Express.js, React.js, Node.js) stack, providing a seamless and user-friendly experience for HR professionals and employees.",
+            "Developing Cross-Platform Mobile App with Flutter:\n" +
+            "At FILUP, I contributed to the development of a mobile app using Flutter, enabling HR platform accessibility on both Android and iOS devices. The app's intuitive UI/UX enhanced employee engagement and efficiency.",
+            "Integrating Advanced HR Functionality and Analytics:\n" +
+            "As part of the FILUP team, I integrated advanced HR functionalities into both the web and mobile platforms, such as applicant tracking, performance evaluations, and data analytics, empowering HR teams with actionable insights for effective decision-making.",
+        ],
+    },
+    {
+        title: "Full Stack Developer & Mobile",
+        company_name: "IPN (Instituto Politécnico Nacional)",
+        icon: ipn,
+        iconBg: "#E6DEDD",
+        date: "Jan 2019 - Sep 2020",
+        points: [
+            "Delivering Customized Solutions for Government Clients:\n" +
+            "As a developer at IPN, I collaborated with a diverse team to design and deliver customized software solutions tailored to the specific needs of government clients. These solutions addressed complex challenges and streamlined various governmental processes.",
+            "Ensuring Security and Compliance in Government Projects:\n" +
+            "At IPN, I emphasized security and compliance throughout the software development lifecycle to meet stringent government standards. My focus on data protection and secure coding practices contributed to the success of various government projects.",
+            "Optimizing Software Development Processes for Efficiency:\n" +
+            "As part of the IPN software factory, I actively participated in process improvement initiatives, leveraging agile methodologies and modern development tools to enhance efficiency, reduce lead times, and ensure on-time delivery for government clients' projects."
+        ],
+    },
+    {
+        title: "Front-end Developer",
+        company_name: "Bankcoin Global",
+        icon: bankcoin,
+        iconBg: "#E6DEDD",
+        date: "Jan 2019 - Sep 2020",
+        points: [
+            "Building Responsive User Interfaces with React and Redux:\n" +
+            "As a developer at BankCoin, I utilized React and Redux to create responsive and intuitive user interfaces for the cryptocurrency exchange platform. By effectively managing state and implementing reusable components, I contributed to a seamless trading experience for our users.",
+            "Integrating Cryptocurrency APIs and Exchange Functionality:\n" +
+            "At BankCoin, I played a key role in integrating various cryptocurrency APIs, enabling real-time market data, trading functionalities, and secure transactions. Through meticulous testing and debugging, I ensured a reliable and smooth exchange platform.",
+            "Implementing Security Measures for Cryptocurrency Transactions:\n" +
+            "As part of the BankCoin team, I prioritized security in cryptocurrency transactions. Utilizing encryption techniques and following best practices, I helped safeguard user assets and data, fostering trust and confidence among our exchange users."
+        ],
+    },
+    {
+        title: "PHP Developer",
+        company_name: "Dot Row Solutions",
+        icon: dotRow,
+        iconBg: "#E6DEDD",
+        date: "Jan 2015 - Sep 2016",
+        points: [
+            "Developed web applications using PHP and CakePHP frameworks.",
+            "Created dynamic web pages and utilized content management systems (CMS) like\n" +
+            "Drupal, WordPress, and Joomla.",
+            "Employed relational databases with MySQL and followed MVC programming principles."
+        ]
+    },
+];
+
+const testimonials = [
+    {
+        testimonial:
+            "Outstanding front-end skills! Marco Antonio exceeded our expectations, delivering a pixel-perfect website. Highly recommended!",
+        name: "Oscar Ardila",
+        designation: "Project Manager",
+        company: "OZON",
+        image: "https://img.freepik.com/vector-gratis/avatar-personaje-empresario-aislado_24877-60111.jpg?w=2000",
+    },
+    {
+        testimonial:
+            "Impressive full-stack expertise! Marco Antonio brought our app to life using the MERN stack. We look forward to future collaborations!",
+        name: "YALO",
+        designation: "Carlos Narez",
+        company: "YALO",
+        image: "https://img.freepik.com/vector-gratis/avatar-personaje-empresario-aislado_24877-60111.jpg?w=2000",
+    },
+    {
+        testimonial:
+            "Highly functional and user-friendly solution! Marco Antonio's professionalism and adherence to deadlines were commendable. Highly recommended!",
+        name: "Jesus Brito",
+        designation: "CTO",
+        company: "Filup MX",
+        image: "https://img.freepik.com/vector-gratis/avatar-personaje-empresario-aislado_24877-60111.jpg?w=2000",
+    },
+];
+
+const projects = [
+    {
+        name: "Car Rent",
+        description:
+            "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+        tags: [
+            {
+                name: "react",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "mongodb",
+                color: "green-text-gradient",
+            },
+            {
+                name: "tailwind",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: carrent,
+        source_code_link: "https://github.com/",
+    },
+    {
+        name: "Job IT",
+        description:
+            "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+        tags: [
+            {
+                name: "react",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "restapi",
+                color: "green-text-gradient",
+            },
+            {
+                name: "scss",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: jobit,
+        source_code_link: "https://github.com/",
+    },
+    {
+        name: "Trip Guide",
+        description:
+            "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+        tags: [
+            {
+                name: "nextjs",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "supabase",
+                color: "green-text-gradient",
+            },
+            {
+                name: "css",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: tripguide,
+        source_code_link: "https://github.com/",
+    },
+];
+
+export {services, technologies, experiences, testimonials, projects};
